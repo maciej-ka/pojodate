2b15606995a539345464f8a9ac6872771dabcf89
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-09-03 09:09:17 +0200

    style: Prettier

--- pojodate.test.ts
+++ pojodate.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest";
 import PojoDate from "./pojodate";
 
-// Zu
 // Jaga
 // Brun
 // Gniewo
+// Zu
 
 // 2016-01-14 08:39:00
 // 2017-09-02 15:20:00
@@ -39,8 +39,16 @@ describe("constructor", () => {
   });
 });
 
-describe('add', () => {
-  it('year', () => {
+describe("toDate", () => {
+  it("works", () => {
+    const actual = new PojoDate("2016-01-14 08:39:00").toDate();
+    expect(actual).toEqual(new Date("2016-01-14 08:39:00"));
+    expect(actual).not.toHaveProperty("pojo");
+  });
+});
+
+describe("add", () => {
+  it("year", () => {
     const actual = new PojoDate("2016-01-14 08:39:00").add({ years: 1 });
     expect(actual).toEqual(new Date("2017-01-14 08:39:00"));
   });
@@ -50,36 +58,28 @@ describe("set", () => {
   it("day", () => {
     const actual = new PojoDate("2024-03-04 19:03:00").set({ days: 1 });
     expect(actual).toEqual(new Date("2024-03-01 19:03:00"));
-  })
-})
+  });
+});
 
-describe('format', () => {
-  it('default', () => {
-    const actual = new PojoDate("2017-09-02 15:20:00").format(d => `${d.days},${d.months},${d.years}`);
+describe("format", () => {
+  it("default", () => {
+    const date = new PojoDate("2017-09-02 15:20:00");
+    const actual = date.format((d) => `${d.days},${d.months},${d.years}`);
     expect(actual).toEqual("02,09,2017");
   });
 
-  it('iso', () => {
+  it("iso", () => {
     const actual = new PojoDate("2020-02-23 14:59:00").formatIso();
     expect(actual).toEqual("2020-02-23 14:59:00");
   });
 });
 
-describe('chains', () => {
-  it('add, set and format', () => {
-    const actual = new PojoDate('2016-01-14')
+describe("chains", () => {
+  it("add, set and format", () => {
+    const actual = new PojoDate("2016-01-14")
       .add({ months: 1 })
       .set({ days: 1 })
-      .format(d => `${d.years}, ${d.months}, ${d.days}`);
-    expect(actual).toEqual('2016, 02, 01');
+      .format((d) => `${d.years}, ${d.months}, ${d.days}`);
+    expect(actual).toEqual("2016, 02, 01");
   });
 });
-
-describe('toDate', () => {
-  it('works', () => {
-    const actual = new PojoDate("2016-01-14 08:39:00").toDate();
-    expect(actual).toEqual(new Date("2016-01-14 08:39:00"));
-    expect(actual).not.toHaveProperty("pojo");
-  });
-});
-
