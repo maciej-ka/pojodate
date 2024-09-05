487deb31e6912c4e86a66c5aec8cf8810eec643e
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-09-05 08:24:07 +0200

    test: Adding minus one day

--- src/pojodate.test.ts
+++ src/pojodate.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest";
 import PojoDate from './pojodate';
 
-// Zu
 // Jaga
-// Brun
+// Buniek
 // Gniewo
+// Zu
 
 // 2016-01-14 08:39:00
 // 2017-09-02 15:20:00
@@ -51,6 +51,11 @@ describe("add", () => {
   it("year", () => {
     const actual = new PojoDate("2016-01-14 08:39:00").add({ years: 1 });
     expect(actual).toEqual(new Date("2017-01-14 08:39:00"));
+  });
+
+  it("day back", () => {
+    const actual = new PojoDate("2024-03-04 19:03:00").add({ days: -1 });
+    expect(actual).toEqual(new Date("2024-03-03 19:03:00"));
   });
 });
 
